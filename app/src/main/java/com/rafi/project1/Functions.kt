b23f1ffbcec22c705baa7fa8082a38f5518b7a57
rafiazharr17 <rafipilo17@gmail.com>
2024-10-12 17:16:21 +0700

    commit 17 : membuat fungsi main dengan memberikan nilai parameter jika fungsi memiliki parameter

--- app/src/main/java/com/rafi/project1/Functions.kt
+++ app/src/main/java/com/rafi/project1/Functions.kt
@@ -10,3 +10,34 @@ fun withParameter(name: String){
     println("== withParameter ==")
     println("Hello, $name")
 }
+
+// Named argument
+// Named argument adalah cara yang memanggil fungsi dengan menyebutkan nama parameter
+
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println("== withNamedArgument ==")
+    println("Hello $name, your $age years old")
+}
+
+// Default parameter value
+// Default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = "rafi", age: Int) {
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello $name, your $age years old")
+}
+
+fun withReturn(panjang : Int, lebar : Int) {
+    println()
+    println("== Panjang * Lebar ==")
+    return println(panjang * lebar)
+}
+
+fun main() {
+    withoutParameter()
+    withParameter("Rafi")
+    withNamedArgument(name = "Azhar", age = 21)
+    withDefaultParameter(age = 21)
+    withReturn(4,5)
+}
