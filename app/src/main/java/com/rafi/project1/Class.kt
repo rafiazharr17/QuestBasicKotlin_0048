23b850eac0c3ced9a4bab5e56b8a2e69d4f8ac75
rafiazharr17 <rafipilo17@gmail.com>
2024-10-12 15:54:09 +0700

    commit 4 : mencetak nilai email, lalu memperbarui nilai email dengan contact.email, lalu di print lagi

--- app/src/main/java/com/rafi/project1/Class.kt
+++ app/src/main/java/com/rafi/project1/Class.kt
@@ -6,4 +6,13 @@ class Contact(val id: Int, var email: String)
 
 fun main(){
     val contact = Contact(1, "rafipilo17@gmail.com")
+
+    // Mencetak nilai properti: email
+    println(contact.email)
+
+    // Memperbarui nilai properti: email
+    contact.email = "rafi@gmail.com"
+
+    // Mencetak nilai baru dari properti: email
+    println(contact.email)
 }
