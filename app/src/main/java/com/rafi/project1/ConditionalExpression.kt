afed1d07878432e903386fec4597942ce4da4b1b
rafiazharr17 <rafipilo17@gmail.com>
2024-10-12 16:33:15 +0700

    commit 9 : membuat fungsi conditional statement menggunakan if-else, when, dan try-catch

---
+++ app/src/main/java/com/rafi/project1/ConditionalExpression.kt
@@ -0,0 +1,38 @@
+package com.rafi.project1
+
+// Conditional Expression
+// Condotional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional expression menggunakan if-else, when, dan try-catch
+
+fun ConditionalStatement(){
+    println("=== Conditional Expression ===")
+    // if-else
+    val number = 10
+    if (number > 0) {
+        println("Angka positif")
+    } else {
+        println("Angka negatif")
+    }
+
+    // when
+    val day = 1
+    when (day) {
+        1 -> println("Senin")
+        2 -> println("Selasa")
+        3 -> println("Rabu")
+        4 -> println("Kamis")
+        5 -> println("Jumat")
+        6 -> println("Sabtu")
+        7 -> println("Minggu")
+        else -> println("Tidak valid")
+    }
+
+    // Try-catch
+    val value = "10"
+    try {
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException) {
+        println("Invalid Number")
+    }
+}
