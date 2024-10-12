30e431dfbd0be0208248a68f252b2a2a3e007afb
rafiazharr17 <rafipilo17@gmail.com>
2024-10-12 16:15:54 +0700

    commit 6 : membuat set dengan setOf() read-only dan mutableSetOf() yang dapat diubah, lalu .add untuk menambahkan data, dan .remove untuk menghapus data. kemudian membuat list yang bersifat lock/read-only untuk mencegah modifikasi yang tidak diingankan

--- app/src/main/java/com/rafi/project1/Collections.kt
+++ app/src/main/java/com/rafi/project1/Collections.kt
@@ -33,3 +33,34 @@ fun ContohList()  {
     val shapesLocked: List<String> = shape
     println(shapesLocked)
 }
+
+// Set adalah kumpulan data yang tidak memiliki posisi atau urutan tertentu dan hanya menyimpan data unik(tidak ada duplikasi)
+// Set bersifat unordered, artinya data yang dimasukan tidak memiliki posisi tertentu
+// Set bersifat mutable dan read only, artinya data yang dimasukan dapat diubah dan tidak dapat diubah
+// Set Read-Only menggunakan setOf
+// Set Mutable menggunakan mutableSetOf
+
+fun ContohSet() {
+    println()
+    println("=== Set ==")
+
+    // Set Read-Only
+    val readOnlyAbjad = setOf("X", "Y", "Z")
+    println(readOnlyAbjad)
+
+    // Set Mutable
+    val shape: MutableSet<String> = mutableSetOf("Futsal", "Basket", "Voli")
+    println(shape)
+
+    // Menambahkan data ke set mutable
+    shape.add("Futsal")
+    println(shape)
+
+    // Menghapus data dari set mutable
+    shape.remove("Basket")
+    println(shape)
+
+    // Set Read-Only
+    val shapesLocked: Set<String> = shape
+    println(shapesLocked)
+}
